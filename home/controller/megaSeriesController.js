1a3dd8c5ebe6372f9bb37f141c27a0628edb44eb
Thalys <thalys.gadelha@ccc.ufcg.edu.br>
2017-06-27 11:17:58 -0300

    Criadas as partes relacionadas ao perfil, falta o código

--- home/controller/megaSeriesController.js
+++ home/controller/megaSeriesController.js
@@ -1,14 +1,15 @@
 angular.module("MegaSeries", []).controller("megaSeriesCtrl", function($scope,$http) {
 
-    $scope.listSer = null;
+    $scope.series = [];
+    $scope.perfil = [];
     
     //funcao que retorna uma lista com as series relacionadas
     $scope.searchSeries = function(nome) {   
         $http.get("https://omdbapi.com/?s=" + nome + "&apikey=93330d3c").then(function(response) {
-                $scope.listSer = response.data.Search; 
-                console.log($scope.listSer);
-        }, function (error) {
-        
-        });
+                $scope.series = response.data.Search; 
+                console.log($scope.series);
+        },  function (error) {
+        	
+            });
     }
 });
---
+++ profile/profile.component.js
@@ -0,0 +1,5 @@
+angular.module('profile').component('profile', {
+	controller: [
+		function 
+	]
+});
